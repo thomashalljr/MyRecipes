c0dd8b66cddbdcc41528368421623aefcd04be19
Thomas Hall Jr <22609450+thomashalljr@users.noreply.github.com>
2023-02-26 19:32:45 -0500

    Devise validations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,8 +1,4 @@
 class Chef < ApplicationRecord
-  before_save { self.email = email.downcase }
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,10 +10,7 @@ class Chef < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   validates :chefname, presence: true, length: {maximum: 30}
-  validates :email, presence: true, length: {maximum: 255},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :email, length: {maximum: 255}
 
   scope :chefname_asc, -> { order("lower(chefname) asc") }
 end
